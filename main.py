0bed6950f54818cacd7e365663b2f3bc72658877
satvik-g4s <satvik.pruthi@in.g4s.com>
2026-03-02 00:52:35 +0530

    Adding Password

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ from supabase import create_client
 from io import BytesIO
 import io
 st.set_page_config(layout="wide")
+def check_password():
+
+    def password_entered():
+        if st.session_state["password"] == st.secrets["APP_PASSWORD"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input(
+            "Enter Password",
+            type="password",
+            on_change=password_entered,
+            key="password",
+        )
+        st.stop()
+
+    elif not st.session_state["password_correct"]:
+        st.text_input(
+            "Enter Password",
+            type="password",
+            on_change=password_entered,
+            key="password",
+        )
+        st.error("Incorrect Password")
+        st.stop()
+
+check_password()
+
 st.title("Vehicle-Distance-Report")
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
