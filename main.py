e8cfe322dd7fe793b858e9e4fa824ae45cc06c6a
satvik-g4s <satvik.pruthi@in.g4s.com>
2026-03-12 13:41:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,36 +89,6 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-def check_password():
-
-    def password_entered():
-        if st.session_state["password"] == st.secrets["APP_PASSWORD"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        st.text_input(
-            "Enter Password",
-            type="password",
-            on_change=password_entered,
-            key="password",
-        )
-        st.stop()
-
-    elif not st.session_state["password_correct"]:
-        st.text_input(
-            "Enter Password",
-            type="password",
-            on_change=password_entered,
-            key="password",
-        )
-        st.error("Incorrect Password")
-        st.stop()
-
-check_password()
-
 st.title("Vehicle-Distance-Report")
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
